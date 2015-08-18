355ba6cc7897ff357c7ffdd8890a60ee571a3f87
Sara Gorecki <sgorecki@gmail.com>
2015-08-18 00:13:30 -0400

    Add ability to back a project, and basic test.

--- tests/unit/pledges_test.js
+++ tests/unit/pledges_test.js
@@ -3,32 +3,50 @@
 var chai = require('chai');
 var expect = chai.expect;
 var bookshelf = require('../../lib/utils/bookshelf');
-var projects = require('../../lib/controllers/project');
+var project = require('../../lib/controllers/project');
+var pledge = require('../../lib/controllers/pledge');
+var Pledge = require('../../lib/models/Pledge');
 
 describe('Backers are able to contribute to a project.', function() {
 
-  afterEach(function(done){
-    bookshelf.knex.raw('truncate projects')
-      .then(function(){
-        bookshelf.knex.raw('truncate pledges')
-      })
-      .then(function(){
-        done();
-      });
+  // before(function(done){
+  //   project.createProject('Super-Project', 2000)
+  //     .then(function() {
+  //       done();
+  //     });
+  // })
 
-  });
+  // after(function(done){
+  //   bookshelf.knex.raw('truncate pledges')
+  //     .then(function(){
+  //       bookshelf.knex.raw('truncate projects')
+  //     })
+  //     .then(function(){
+  //       done();
+  //     });
+
+  // });
 
   it ('Should return a promise that resovles to a pledge.', function(done) {
-    projects.createProject('Super-Project', 2000);
-    var pledgePromise = backProject('Tom', 'Super-Project', 4111111111111111, 20);
-    expect(pledge.then).to.be.a('function');
-    pledgePromise.then(function(pledge) {
-      expect(pledge).to.be.an('object');
-      expect(pledge.backer).to.equal('Tom');
-      expect(pledge.project).to.equal('Super-Project');
-      expect(pledge.creditCard).to.equal('4111111111111111');
-      expect(pledge.amount).to.equal(20);
-    });
+
+      var pledgePromise = pledge.backProject('Tom', 'Super-Project', 12345, 20)
+        .then(function(pledge) {
+          expect(pledge).to.be.an('object');
+          expect(pledge.get('backer')).to.equal('Tom');
+          expect(pledge.get('creditCard')).to.equal(12345);
+          expect(pledge.get('amount')).to.equal(20);
+          return pledge.get('id');
+        })
+        .then(function(pledgeId) {
+          return new Pledge({id: pledgeId})
+            .fetch()
+            .then(function(pledge){
+              expect(pledge).to.exist;
+            });
+        })
+        .then(function() {
+          done();
+        });
   });
 
 });
