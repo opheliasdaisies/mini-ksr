0703d11618178a8ba9a48c72a240c6085d69f6df
Sara Gorecki <sgorecki@gmail.com>
2015-08-23 23:42:10 -0400

    Add luhn credit card check.

--- tests/unit/pledges_test.js
+++ tests/unit/pledges_test.js
@@ -28,11 +28,11 @@ describe('Backers are able to contribute to a project.', function() {
   });
 
   it ('Should return a promise that resovles to a pledge.', function() {
-    return pledge.backProject('Thomas', 'Super-Project', '12345', 20)
+    return pledge.backProject('Thomas', 'Super-Project', '4111111111111111', 20)
       .then(function(pledge) {
         expect(pledge).to.be.an('object');
         expect(pledge.get('backer')).to.equal('Thomas');
-        expect(pledge.get('creditCard')).to.equal('12345');
+        expect(pledge.get('creditCard')).to.equal('4111111111111111');
         expect(Number(pledge.get('amount'))).to.equal(20);
         return pledge.get('id');
       })
@@ -44,54 +44,58 @@ describe('Backers are able to contribute to a project.', function() {
       });
   });
 
-  it ('Should return a promise that resovles to an error if the project doesn\'t exist.', function() {
-    var pledgePromise = pledge.backProject('Jackelyn', 'OMG-A-Project', '12345', 1000);
+  it ('Should return a promise that rejects with an error if the project doesn\'t exist.', function() {
+    var pledgePromise = pledge.backProject('Jackelyn', 'OMG-A-Project', '4111111111111111', 1000);
     var expectedError = 'You must supply the name of a valid project to back it.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resovles to an error if arguments are missing.', function() {
-    var pledgePromise = pledge.backProject('Jackelyn', 'Super-Project', '50');
+  it ('Should return a promise that rejects with an error if arguments are missing.', function() {
+    var pledgePromise = pledge.backProject('Jackelyn', 'Super-Project', '4111111111111111');
     var expectedError = 'You must supply the dollar amount you want to put towards the project.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resoves to an error if backer name includes invalid characters.', function() {
-    var pledgePromise = pledge.backProject('Not A Backer', 'Super-Project', '12345', 1000);
+  it ('Should return a promise that rejects with an error if backer name includes invalid characters.', function() {
+    var pledgePromise = pledge.backProject('Not A Backer', 'Super-Project', '4111111111111111', 1000);
     var expectedError = 'Backer names can only include alphaneumeric characters, dashes, and underscores.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the backer\'s name is less than 4 characters', function() {
-    var pledgePromise = pledge.backProject('Me', 'Super-Project', '12345', 1000);
+  it ('Should return a promise that rejects with an error if the backer\'s name is less than 4 characters', function() {
+    var pledgePromise = pledge.backProject('Me', 'Super-Project', '4111111111111111', 1000);
     var expectedError = 'Backer names must be longer than 3 characters.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the backer\'s name is more than 20 characters', function() {
-    var pledgePromise = pledge.backProject('Everybody-Wants-To-Back-A-Project', 'Super-Project', '12345', 1000);
+  it ('Should return a promise that rejects with an error if the backer\'s name is more than 20 characters', function() {
+    var pledgePromise = pledge.backProject('Everybody-Wants-To-Back-A-Project', 'Super-Project', '4111111111111111', 1000);
     var expectedError = 'Backer names can not be longer than 20 characters.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if credit card numbers are longer than 19 characters.', function() {
-    var pledgePromise = pledge.backProject('Ida_Backer', 'Super-Project', '12121941847283719261249286e12', 1000);
+  it ('Should return a promise that rejects with an error if credit card numbers are longer than 19 characters.', function() {
+    var pledgePromise = pledge.backProject('Ida_Backer', 'Super-Project', '212121418472837192612491612', 1000);
     var expectedError = 'Credit card numbers can not be longer than 19 characters.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if credit card numbers are not numeric.', function() {
-    var pledgePromise = pledge.backProject('Ida_Backer', 'Super-Project', '871hi8a02', 1000);
+  it ('Should return a promise that rejects with an error if credit card numbers are not numeric.', function() {
+    var pledgePromise = pledge.backProject('Ida_Backer', 'Super-Project', '871 802', 1000);
     var expectedError = 'Credit cards must contain only numeric characters.';
     return promiseIsExpectedError(pledgePromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if credit card numbers do not pass Luhn-10 validation.');
+  it ('Should return a promise that rejects with an error if credit card numbers do not pass Luhn-10 validation.', function() {
+    var pledgePromise = pledge.backProject('Ida_Backer', 'Super-Project', '4111111111111112', 1000);
+    var expectedError = 'Entered credit card number is invalid.';
+    return promiseIsExpectedError(pledgePromise, expectedError);
+  });
 
-  it ('Should return a promise that resolves to an error if the credit card number entered has already been used.');
+  it ('Should return a promise that rejects with an error if the credit card number entered has already been used.');
 
   it ('Should have a pledge value that accepts dollars and cents.');
 
-  it ('Should return a promise that resolves to an error if the target is preceeded with $');
+  it ('Should return a promise that rejects with an error if the target is preceeded with $');
 
 });
--- tests/unit/projects_test.js
+++ tests/unit/projects_test.js
@@ -35,25 +35,25 @@ describe('A new project can be created with a project name and a target dollar a
       });
   });
 
-  it ('Should return a promise that resolves to an error if the project\'s name includes invalid characters.', function() {
+  it ('Should return a promise that rejects with an error if the project\'s name includes invalid characters.', function() {
     var projectPromise = projects.createProject('Project-%', 200);
     var expectedError = 'Project names can only include alphaneumeric characters, dashes, and underscores.';
     return promiseIsExpectedError(projectPromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the project\'s name is less than 4 characters.', function() {
+  it ('Should return a promise that rejects with an error if the project\'s name is less than 4 characters.', function() {
     var projectPromise = projects.createProject('The', 500);
     var expectedError = 'Your project name must be longer than 3 characters.';
     return promiseIsExpectedError(projectPromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the project\'s name is more than 20 characters.', function() {
+  it ('Should return a promise that rejects with an error if the project\'s name is more than 20 characters.', function() {
     var projectPromise = projects.createProject('The_Greatest_Project_To_Ever_Exist', 5000000000);
     var expectedError = 'Your project name can not be longer than 20 characters.';
     return promiseIsExpectedError(projectPromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the target is not given.', function() {
+  it ('Should return a promise that rejects with an error if the target is not given.', function() {
     var projectPromise = projects.createProject('Our-Project');
     var expectedError = 'You must supply a target value for your project.';
     return promiseIsExpectedError(projectPromise, expectedError);
@@ -66,13 +66,13 @@ describe('A new project can be created with a project name and a target dollar a
     });
   });
 
-  it ('Should return a promise that resolves to an error if the target is preceeded with $', function() {
+  it ('Should return a promise that rejects with an error if the target is preceeded with $', function() {
     var projectPromise = projects.createProject('MoneyProject', '$500');
     var expectedError = 'You must enter a number for your project\'s target. Do not include a $ sign.';
     return promiseIsExpectedError(projectPromise, expectedError);
   });
 
-  it ('Should return a promise that resolves to an error if the project name already exists', function() {
+  it ('Should return a promise that rejects with an error if the project name already exists', function() {
       var projectPromise = projects.createProject('A-Project', 200000);
       var expectedError = 'A project with the name A-Project already exists. Please try another name.'
       return promiseIsExpectedError(projectPromise, expectedError);
