9c93b91a79b77d14d9b92d18f7d448fe947e7bdc
Sara Gorecki <sgorecki@gmail.com>
2015-08-29 18:58:18 -0400

    Add yargs to parse command line arguments

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function startKickstarter(){
-  process.stdout.write('Welcome to Mini-Kickstarter!');
-}
+var argv = require('yargs');
+var subHelpOptions = require('./lib/utils/cmdLineHelpFunctions');
+var inputDispatcher = require('./lib/controllers/inputDispatcher');
 
-startKickstarter();
+var args = argv.argv._;
+var command = args.shift();
+
+argv
+  .usage('Usage: $0 <command> [arguments]')
+  .command('project', 'Create a new project.\nTakes project name and target value as arguments.\n', subHelpOptions.project)
+  .command('back', 'Back a project.\nTakes backer name, project name, credit card number, and pledge amount as arguments.\n', subHelpOptions.back)
+  .command('list', 'List all pledges towards a project.\nTakes project name as argument.\n', subHelpOptions.list)
+  .command('backer', 'List all pledges a backer has made.\nTakes backer name as argument.\n', subHelpOptions.backer)
+  .demand(2, 5, 'Please use a valid command and supply the correct number of arguments. The -help flag will provide more information for any given command.\n')
+  .string('_')
+  .example('$0 project Project_Sugar_Cube 500', 'Create a project with the given project name and target goal.\n')
+  .example('$0 back John Project_Sugar_Cube 4111111111111111 20', 'Back a given project with a credit card and an amount to pledge.\n')
+  .example('$0 list Project_Sugar_Cube', 'List all pledges towards a given project.\n')
+  .example('$0 backer John', 'List all the projects a backer has contributed to.\n')
+  .help('h')
+  .alias('h', 'help')
+  .choices(command, ['project', 'back', 'list', 'backer'])
+  .wrap(argv.terminalWidth())
+  .argv;
+
+inputDispatcher(command, args);
