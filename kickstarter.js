87054db387734c021435f6dbe2fa57a8ba398c3a
Sara Gorecki <sgorecki@gmail.com>
2015-08-31 02:49:41 -0400

    Add commetns to kickstarter

--- kickstarter.js
+++ kickstarter.js
@@ -7,6 +7,7 @@ var inputDispatcher = require('./lib/cli/inputDispatcher');
 var sequelize = require('./lib/utils/sequelize');
 var Promise = sequelize.Promise;
 
+// Parse arguments and options provided by user via command line.
 var argv = yargs
   .usage('Usage: $0 <command> [arguments]')
   .command('project', 'Create a new project.\nTakes project name and target value as arguments.\n')
@@ -35,26 +36,37 @@ var argv = yargs
   })
   .argv;
 
+// argv._ is an array of all non-hyphenated arguments supplied.
 var args = argv._;
+// The first will always be the command, and the following will be the arguments to be passed.
 var command = args.shift();
 
+// Pass the user input to the inputDispatcher module.
 function takeUserInput(cmd, argArr){
   return inputDispatcher(cmd, argArr)
+    // If the command doesn't exist, show help.
     .catch(function(){
+      // If the -s flag was used, do not show help, as the user may have only synced the tables without adding a command.
       if (!argv.s){
         yargs.showHelp();
       }
     })
     .finally(function(){
+      // Close the connection to the database.
       sequelize.close();
     });
 }
 
+// If -s is included, the database tables will be created. If -sf is included, the tables will be deleted and then re-created. If -s is not used, a promise will be returned.
 function checkSyncOptions(argv){
   if (argv.s) {
+    // Check for force option.
     var options = (argv.f ? {force: true} : {});
     var message = 'Tables have been created ';
+    // Adjust message to be printed based on if force option was added.
     message += (argv.f ? 'or overwritten.' : 'if they did not exist.');
+
+    // Sync the database tables
     return sequelize.sync(options)
       .then(function(){
         console.log(message);
